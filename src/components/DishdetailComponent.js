c9a32bfcbaae332dc60fb699e5cf84c104e29253
Vadim Mostovenko <vadim.mostovenko@gmail.com>
2020-06-10 18:35:02 +0300

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 	 Button, Modal, ModalHeader, ModalBody, Row, Label } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -130,8 +131,25 @@ const minLength = (len) => (val) => val && (val.length >= len);
     }
 
 	const DishDetail = (props) => {
-		const dish = props.dish;
-		if (dish != null) {
+		if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null)  {
 			return (
                 <div className="container">
 					<div className="row">
